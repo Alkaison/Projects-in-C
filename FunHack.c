018bf4db2fad6f53171bef97ce99188346355a77
Ganesh Mourya <98116504+Alkaison@users.noreply.github.com>
2022-09-28 20:46:42 +0530

    Add files via upload

--- FunHack.c
+++ FunHack.c
@@ -4,16 +4,14 @@
 int main(){
     
     int acc;
-    
-    //clears the terminal screen for better visibility
+
     system("cls");
     
-    printf("Congratulations, You won 7 cr. prize! \n");
+    printf("Congo, You won 7 cr. prize! \n");
     printf("Enter your A/c no.: ");
     scanf("%d",&acc);
 
     printf("\nProcessing your request... \n");
-    //adds delay of 4s before moving to next line
     Sleep(4000);
 
     printf("\nWaiting for server side response... \n");
@@ -22,19 +20,13 @@ int main(){
     printf("\nAlmost done... \n");
     Sleep(2000);
 
-    //this changes the terminal color to GREEN
     system("color a");
-    
-    //this changes the command prompt header text
     system("title Hacking Bank A/c...");
-    
-    //clears the terminal screen for better visibility
+    system("prompt Root@126.61.23.01");
     system("cls");
 
-    //infinity loop
     for(int i=0; ; i++)
     {
-        //lists the current directory files
         system("dir/s");
     }
 
