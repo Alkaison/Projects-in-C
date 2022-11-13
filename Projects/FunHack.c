c66edd1c7d8d51f4159653a1ac5289d288099cde
Alkaison <505ganeshmourya@gmail.com>
2022-11-13 17:36:58 +0530

    improved FunHack code

--- Projects/FunHack.c
+++ Projects/FunHack.c
@@ -4,23 +4,22 @@
 int main(){
     
     int acc;
-
     system("cls");
     
-    printf("Congo, You won 7 cr. prize! \n");
-    printf("Enter your A/c no.: ");
+    printf("Congratulations, You won 7 crore cash prize!! \n");
+    printf("Enter your A/c no. to claim the amount: ");
     scanf("%d",&acc);
 
     printf("\nProcessing your request... \n");
-    Sleep(4000);
+    Sleep(2500);
 
     printf("\nWaiting for server side response... \n");
-    Sleep(3000);
-
-    printf("\nAlmost done... \n");
-    Sleep(2000);
+    Sleep(1500);
 
     system("color a");
+    printf("\nAlmost done... \n");
+    Sleep(500);
+
     system("title Hacking Bank A/c...");
     system("prompt Root@126.61.23.01");
     system("cls");
