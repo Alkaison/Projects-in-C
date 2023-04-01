b64d93d6d5ee7fed261f0fc84226656707a910db
Alkaison <505ganeshmourya@gmail.com>
2023-04-01 11:04:17 +0530

    Refactor: Calculator

--- Projects/Calculator.c
+++ Projects/Calculator.c
@@ -1,103 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//Function Prototypes
-void printSum(float, float);
-void printDiff(float, float);
-void printProd(float, float);
-void printDiv(float, float);
-void printModu(int, int);
-
-int main(){
-
+int main()
+{
     float num1, num2;
     char operator, retry;
 
-//if user says he want to try again, the program will restart from here...
-label1:
-
-    //clears the terminal screen
-    system("cls"); 
-
-    printf("Enter the operator (+ - * / %) : ");
-    scanf(" %c", &operator);
-
-    //check the input for operators
-    if(operator=='+' || operator=='-' || operator=='*' || operator=='/' || operator=='%')
+    do
     {
+        // clears the terminal screen
+        system("cls");
+
+        printf("Enter the operator (+ - * /) : ");
+        scanf(" %c", &operator);
+
+        // clears input buffers in console 
+        fflush(stdin);
+
         printf("Enter first number: ");
         scanf("%f", &num1);
 
         printf("Enter second number: ");
         scanf("%f", &num2);
 
-        //switch case for getting result/output
-        switch(operator) 
+        // switch case for getting result/output
+        switch (operator)
         {
             case '+':
-                printSum(num1, num2);
+                printf("\nSum of %.0f & %.0f is: %.0f \n", num1, num2, (num1 + num2));
                 break;
             case '-':
-                printDiff(num1, num2);
+                printf("\nSubtraction of %.0f & %.0f is: %.0f \n", num1, num2, (num1 - num2));
                 break;
             case '*':
-                printProd(num1, num2);
+                printf("\nProduct of %.0f & %.0f is: %.0f \n", num1, num2, (num1 * num2));
                 break;
             case '/':
-                printDiv(num1, num2);
-                break;
-            case '%':
-                printModu(num1, num2);
+                printf("\nDivision of %.0f & %.0f is: %.0f \n", num1, num2, (num1 / num2));
                 break;
             default:
-                printf("ERROR: Invalid Input.");
+                printf("\nERROR: Invalid Input. \n");
         }
-    } 
-    else 
-    {
-        printf("ERROR: Invalid Input.");
-        //this will end the program here itself if the operator input is invaild
-        exit(0);
-    }
-    
-    //checks if user wants to use calculator again 
-    printf("\nDo you waana try again [y/N]: ");
-    scanf(" %c", &retry);
 
-    if(retry=='y' || retry=='Y')
-    {
-        goto label1;
-    } 
-    else if(retry=='n' || retry=='N')
-    {
-        printf("Program Ended.");
-    }
-    else
-    {
-        printf("ERROR: Invalid Input.");
-    }
-    
+        // clears input buffers in console 
+        fflush(stdin);
+
+        // checks if user wants to use calculator again
+        printf("\nDo you waana try again [y/N]: ");
+        scanf(" %c", &retry);
+    } while (retry == 'y' || retry == 'Y');
+
     return 0;
-}
-
-//Function Bodies
-void printSum(float num1, float num2) {
-    printf("Sum of %.0f & %.0f is: %.0f", num1, num2, (num1+num2));
-}
-
-void printDiff(float num1, float num2){
-    printf("Subtraction of %.0f & %.0f is: %.0f", num1, num2, (num1-num2));
-}
-
-void printProd(float num1, float num2){
-    printf("Product of %.0f & %.0f is: %.0f", num1, num2, (num1*num2));
-}
-
-void printDiv(float num1, float num2){
-    printf("Division of %.0f & %.0f is: %.0f", num1, num2, (num1/num2));
-}
-
-void printModu(int num1, int num2){
-    int modu = num1 % num2;
-    printf("Modules of %d & %d is: %d", num1, num2, modu); 
 }
