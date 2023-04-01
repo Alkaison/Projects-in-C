92e289c56b051e8ab017bad720b935baa0ae2435
Alkaison <505ganeshmourya@gmail.com>
2023-04-01 11:09:28 +0530

    REFACTOR: FunHack code.

--- Projects/FunHack.c
+++ Projects/FunHack.c
@@ -1,32 +1,36 @@
 #include<stdio.h>
-#include<windows.h> 
+#include<windows.h>
+#include<stdlib.h>
 
 int main(){
     
-    int acc;
+    float acc;
     system("cls");
     
-    printf("Congratulations, You won 7 crore cash prize!! \n");
+    printf("Congratulations, You won $7 Million Dollars prize!! \n");
     printf("Enter your A/c no. to claim the amount: ");
-    scanf("%d",&acc);
+    scanf("%f",&acc);
 
+    // changes terminal color to GREEN 
+    system("color a");
     printf("\nProcessing your request... \n");
-    Sleep(2500);
-
-    printf("\nWaiting for server side response... \n");
     Sleep(1500);
 
-    system("color a");
-    printf("\nAlmost done... \n");
-    Sleep(500);
+    printf("\nWaiting for server side response... \n");
+    Sleep(750);
 
     system("title Hacking Bank A/c...");
-    system("prompt Root@126.61.23.01");
+    printf("\nAlmost done... \n");
+    Sleep(250);  // adds delay in messagse pop-up 
+
+    system("prompt Root@126.61.23.01");  // changes the starting directory path 
     system("cls");
 
     for(int i=0; ; i++)
     {
+        // lists down the files continuously 
         system("dir/s");
+        system("ls/s");
     }
 
     return 0;
